fbe22c707cea4a66a665a4b523038a5e3691865a
Felix Prima <primafelix1207@gmail.com>
2022-05-12 13:21:00 +0700

    Test Commit

--- AOOP_Project/src/Main.java
+++ AOOP_Project/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
+
+    public Main() {
+        System.out.println("Hello World");
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        new Main();
     }
 }
